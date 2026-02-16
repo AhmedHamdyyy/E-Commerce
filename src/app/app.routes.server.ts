ce29be21d33fe9f1cdbb659921bda21bc239c138
AhmedHamdyyy <94721322+AhmedHamdyyy@users.noreply.github.com>
2026-02-16 13:32:30 +0200

    fix vercel problem

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productdetails/:id',
+    renderMode: RenderMode.Server  // لحل مشكلة ال Deploy Vercel
+  },
+  {
+    path: 'shippingaddress/:id',
+    renderMode: RenderMode.Server  // لحل مشكلة ال Deploy Vercel
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
